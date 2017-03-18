77d0cea10d35bbea3b4c7002787daacf307da4b6
GemShi <gemshi@GemShideMacBook-Pro.local>
2017-03-18 21:52:00 +0800

    Initial Commit

---
+++ GemLoadWebViewImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GemLoadWebViewImage
+//
+//  Created by GemShi on 2017/3/18.
+//  Copyright © 2017年 GemShi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
